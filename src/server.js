e566fc24e0f673c6465f902bf81b6832c6320542
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-10-08 21:11:42 -0400

    dynamic website assignment

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 // use the express library
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // create a new server application
 const app = express();
+
+// middleware
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(cookieParser());
 
 // Define the port we will listen on
 // (it will attempt to read an environment global
@@ -11,9 +17,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+let nextVisitorId = 1;
+
 // The main page of our website
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let lastvisitsecond = req.cookies.visited ? (parseInt((Date.now() - req.cookies.visited)/1000)) : -1;
+  let visitorId = !req.cookies.visitorId ? nextVisitorId++ : req.cookies.visitorId;
+
+  res.cookie('visitorId', visitorId);
+  res.cookie('visited', Date.now().toString());
+  res.render('welcome', {
+    name: req.query.name || "World",
+    date: new Date().toLocaleString(),
+    lastvisitsecond,
+    visitorId
+  });
 });
 
 // Start listening for network connections
