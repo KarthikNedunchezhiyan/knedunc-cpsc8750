7657826a72c542f993c92fa49af07a170b9b8358
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-10-31 08:20:46 -0400

    integrated trivia DB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // use the express library
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const fetch = require('node-fetch');
 
 // create a new server application
 const app = express();
@@ -32,6 +33,43 @@ app.get('/', (req, res) => {
     lastvisitsecond,
     visitorId
   });
+});
+
+app.get("/trivia", async (req, res) => {
+  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+  if (!response.ok) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+    return;
+  }
+  const content = await response.json();
+  if (content.response_code !== 0) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+    return;
+  }
+
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+
+  let question_meta = content.results[0];
+  question_meta.answers = question_meta.incorrect_answers;
+  question_meta.answers.push(question_meta.correct_answer);
+
+  shuffle(question_meta.answers);
+
+  // res.send(JSON.stringify(content, 2));
+  res.render('trivia', question_meta);
 });
 
 // Start listening for network connections
